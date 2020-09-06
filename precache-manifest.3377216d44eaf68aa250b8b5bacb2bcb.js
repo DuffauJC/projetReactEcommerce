44129a77b993594f99de81931ed74764daf0ea50
DuffauJC <jeancharles.duffau@gmail.com>
2020-09-06 18:09:53 +0200

    Updates

---
+++ precache-manifest.3377216d44eaf68aa250b8b5bacb2bcb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f88359079425faada79eac1d926b673",
+    "url": "/projetreactEcommerce/index.html"
+  },
+  {
+    "revision": "9476409cf11955dd1f8b",
+    "url": "/projetreactEcommerce/static/css/main.8b8ee9c8.chunk.css"
+  },
+  {
+    "revision": "8e906fb8b0a9b9ffb90b",
+    "url": "/projetreactEcommerce/static/js/2.8302b24d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/projetreactEcommerce/static/js/2.8302b24d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9476409cf11955dd1f8b",
+    "url": "/projetreactEcommerce/static/js/main.de09024b.chunk.js"
+  },
+  {
+    "revision": "02ba93d82d605f103afa",
+    "url": "/projetreactEcommerce/static/js/runtime-main.a7f305b6.js"
+  }
+]);
